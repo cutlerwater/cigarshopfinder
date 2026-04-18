140e430c90113e9b967c05fcf7fa1a0588a81280
Garrett Cutler <cutlerwater2@live.com>
2026-04-17 23:46:38 -0400

    splitting up the states

---
+++ lib/index.ts
@@ -0,0 +1,106 @@
+import { alabamaShops } from "./alabama";
+import { alaskaShops } from "./alaska";
+import { arizonaShops } from "./arizona";
+import { arkansasShops } from "./arkansas";
+import { californiaShops } from "./california";
+import { coloradoShops } from "./colorado";
+import { connecticutShops } from "./connecticut";
+import { washingtondcShops } from "./dc";
+import { delawareShops } from "./delaware";
+import { hawaiiShops } from "./hawaii";
+import { idahoShops } from "./idaho";
+import { illinoisShops } from "./illinois";
+import { indianaShops } from "./indiana";
+import { iowaShops } from "./iowa";
+import { kansasShops } from "./kansas";
+import { kentuckyShops } from "./kentucky";
+import { louisianaShops } from "./louisiana";
+import { maineShops } from "./maine";
+import { marylandShops } from "./maryland";
+import { massachussettsShops } from "./massachussetts";
+import { michiganShops } from "./michigan";
+import { minnesotaShops } from "./minnesota";
+import { mississippiShops } from "./mississippi";
+import { missouriShops } from "./missouri";
+import { montanaShops } from "./montana";
+import { nebraskaShops } from "./nebraska";
+import { nevadaShops } from "./nevada";
+import { newhampshireShops } from "./newhampshire";
+import { newjerseyShops } from "./newjersey";
+import { newmexicoShops } from "./newmexico";
+import { newyorkShops } from "./newyork";
+import { northcarolinaShops } from "./northcarolina";
+import { northdakotaShops } from "./northdakota";
+import { ohioShops } from "./ohio";
+import { oklahomaShops } from "./oklahoma";
+import { oregonShops } from "./oregon";
+import { pennsylvaniaShops } from "./pennsylvania";
+import { rhodeislandShops } from "./rhodeisland";
+import { southcarolinaShops } from "./southcarolina";
+import { southdakotaShops } from "./southdakota";
+import { tennesseeShops } from "./tennessee";
+import { texasShops } from "./texas";
+import { utahShops } from "./utah";
+import { vermontShops } from "./vermont";
+import { virginiaShops } from "./virginia";
+import { washingtonShops } from "./washington";
+import { westvirginiaShops } from "./westvirginia";
+import { wisconsinShops } from "./wisconsin";
+import { wyomingShops } from "./wyoming";
+
+
+export const shops = [
+    ...marylandShops,
+    ...virginiaShops,
+    ...westvirginiaShops,
+    ...hawaiiShops,
+    ...alaskaShops,
+    ...arizonaShops,
+    ...alabamaShops,
+    ...arkansasShops,
+    ...coloradoShops,
+    ...californiaShops,
+    ...washingtonShops,
+    ...newmexicoShops,
+    ...newjerseyShops,
+    ...delawareShops, 
+    ...oregonShops,
+    ...idahoShops,
+    ...montanaShops,
+    ...wyomingShops,
+    ...utahShops,
+    ...nevadaShops,
+    ...connecticutShops,
+    ...maineShops,
+    ...newhampshireShops,
+    ...vermontShops,
+    ...newyorkShops,
+    ...pennsylvaniaShops,
+    ...newjerseyShops,
+    ...delawareShops,
+    ...washingtondcShops,
+    ...northcarolinaShops,
+    ...northdakotaShops,
+    ...southcarolinaShops,
+    ...southdakotaShops,
+    ...michiganShops,
+    ...minnesotaShops,
+    ...missouriShops,
+    ...newmexicoShops,
+    ...ohioShops,
+    ...iowaShops,
+    ...illinoisShops,
+    ...indianaShops,
+    ...mississippiShops,
+    ...texasShops,
+    ...tennesseeShops,
+    ...kentuckyShops,
+    ...nebraskaShops,
+    ...louisianaShops,
+    ...kansasShops,
+    ...oklahomaShops,
+    ...wisconsinShops,
+    ...rhodeislandShops,
+    ...massachussettsShops,
+        
+];
