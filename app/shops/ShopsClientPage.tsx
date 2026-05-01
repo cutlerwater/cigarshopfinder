5e8944d6022f89aea2f8f4cd09c131d0b89af2a0
Garrett Cutler <cutlerwater2@live.com>
2026-05-01 09:32:22 -0400

    Adding the florida shop

--- app/shops/ShopsClientPage.tsx
+++ app/shops/ShopsClientPage.tsx
@@ -154,7 +154,7 @@ const STATES = [
     { name: "Colorado", value: "CO" },
     { name: "Connecticut", value: "CT" },
     { name: "Delaware", value: "DE" },
-    { name: "Florida", value: "FLA" },
+    { name: "Florida", value: "FL" },
     { name: "Georgia", value: "GA" },
     { name: "Hawaii", value: "HI" }, 
     { name: "Idaho", value: "ID" },
